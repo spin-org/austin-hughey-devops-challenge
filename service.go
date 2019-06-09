bfac1a2b451bec10aa8bb560e5b1d3bf1970e7df
J. Austin Hughey <jah@jah.io>
2019-06-08 23:33:42 -0600

    rough iteration of a constant open tcp connection service

---
+++ service.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"os"
+	"io"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+	  port = "8000"
+	}
+
+	fmt.Println("Starting up on port:", port)
+
+	ln, err := net.Listen("tcp", ":" + port)
+	if err != nil {
+		fmt.Println("Error listening on port:", port)
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn, port)
+	}
+}
+
+func handleRequest(conn net.Conn, port string) {
+	// Spit something out on the local server
+	fmt.Println("Received TCP Connection (local <-> remote)", conn.LocalAddr(), " <-> ", conn.RemoteAddr)
+
+	// Tell the user something
+	conn.Write([]byte("Hello! From goservice.go"))
+
+	// What happens if the user closes their connection?
+	_, err := conn.Read(make([]byte, 0))
+	if err != io.EOF {
+		fmt.Println("Remote user closed connection:", conn.RemoteAddr())
+	}
+}
